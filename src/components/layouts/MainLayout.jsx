b611b97a23655d89acfe596b20bef4d7ab9554cb
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2025-07-01 21:52:04 +0600

    add react spinners

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import { BarLoader } from 'react-spinners';
 
 const MainLayout = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex gap-4 items-center justify-center bg-white">
+        <BarLoader color="#CAEB66" height={10} width={200}></BarLoader>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-350 mx-auto p-4">
       <Navbar />
